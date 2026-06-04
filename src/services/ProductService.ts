fb3158238c661e1f6fb460fe05ac6c68a9f12bd0
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-04 11:06:03 -0500

    Creacion de logica de negocio para productos

---
+++ src/services/ProductService.ts
@@ -0,0 +1,75 @@
+
+import {prisma} from "../config/database"
+import {Productos} from "../entities/productos"
+import type {ProductoRequest} from "../types/productos/request"
+import type {ProductoResponse} from "../types/productos/response"
+import { MapperProducto } from "../utils/mappers/MapperProducto"
+
+export class ProductService {
+
+    async list(): Promise<ProductoResponse[]> {
+        const productos = await prisma.productos.findMany({
+            include: {
+                tipo: true,
+                categoria: true,
+            },
+        })
+        
+        return productos.map(p => {
+            const productoEntity = Productos.fromPrisma(p)
+            return MapperProducto.toResponse(
+                productoEntity
+            )
+        })
+    }
+
+    async create(request: ProductoRequest): Promise<ProductoResponse> {
+        const productoEntity = MapperProducto.fromRequest(request)
+        const productoPrisma = await prisma.productos.create({
+            data: {
+                id_producto: productoEntity.id_producto,
+                nombre_producto: productoEntity.nombre_producto,
+                descripcion: productoEntity.descripcion,
+                precio_unitario: productoEntity.precio_unitario,
+                cantidad_stock: productoEntity.cantidad_stock,
+                id_proveedor: productoEntity.id_proveedor,
+                detalles: productoEntity.detalles,
+                id_tipo: productoEntity.id_tipo,
+                id_categoria: productoEntity.id_categoria,
+                fecha_vencimiento: productoEntity.fecha_vencimiento,
+            },
+        })
+        return MapperProducto.toResponse(Productos.fromPrisma(productoPrisma))
+    }
+
+    async delete(id_producto: string): Promise<void> {
+        await prisma.productos.delete({
+            where: {
+                id_producto,
+            },
+        })
+    }
+
+    async update(id_producto: string, request: ProductoRequest): Promise<ProductoResponse> {
+        const productoEntity = MapperProducto.fromRequest(request, id_producto)
+        const productoPrisma = await prisma.productos.update({
+            where: {
+                id_producto,
+            },
+            data: {
+                nombre_producto: productoEntity.nombre_producto,
+                descripcion: productoEntity.descripcion,
+                precio_unitario: productoEntity.precio_unitario,
+                cantidad_stock: productoEntity.cantidad_stock,
+                id_proveedor: productoEntity.id_proveedor,
+                detalles: productoEntity.detalles,
+                id_tipo: productoEntity.id_tipo,
+                id_categoria: productoEntity.id_categoria,
+                fecha_vencimiento: productoEntity.fecha_vencimiento,
+            },
+        })
+        return MapperProducto.toResponse(Productos.fromPrisma(productoPrisma))
+    }
+
+    
+}
