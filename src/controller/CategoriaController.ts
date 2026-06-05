6e0a960600acf4d8ecc12bafa2d3f6f7a4f2f3ae
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-04 22:14:09 -0500

    Controladores de Proveedores, Tipos y Categorias

---
+++ src/controller/CategoriaController.ts
@@ -0,0 +1,11 @@
+import type { Request, Response } from 'express';
+import { CategoriaService } from '../services/CategoriaService';
+
+const service = new CategoriaService();
+
+export class CategoriaController {
+    static async list(_req: Request, res: Response): Promise<void> {
+        const categorias = await service.list();
+        res.json(categorias);
+    }
+}
