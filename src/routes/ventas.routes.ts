ecd1838dc62e2c4949bccd33049e52d4d4dd3b0b
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-07 00:42:51 -0500

    Rutas de ventas

---
+++ src/routes/ventas.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { VentaController } from "../controller/VentaController"
+import { authenticate } from "../middleware/authenticate"
+import { authorize } from "../middleware/authorize"
+
+export const ventasRouter = Router()
+
+ventasRouter.post("/", authenticate, authorize("Empleado"), VentaController.create)
+ventasRouter.get("/", authenticate, authorize("Admin", "Empleado"), VentaController.list)
+ventasRouter.get("/:id_venta", authenticate, authorize("Admin"), VentaController.getById)
