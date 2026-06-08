8681ab8f9792c12d5e9a103942af575e182c02c3
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-08 10:23:29 -0500

    Ruta logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import { AuthController } from "../controller/AuthController"
+import { authenticate } from "../middleware/authenticate"
 
 export const authRouter = Router()
 
 authRouter.post("/", AuthController.login)
+authRouter.post("/logout", authenticate, AuthController.logout)
