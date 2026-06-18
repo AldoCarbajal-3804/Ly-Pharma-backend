13ef4830c2d6206f0d3d58da8e32785a835ca0c6
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-18 14:26:24 -0500

    Arreglando error de SSL

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg"
 import "dotenv/config"
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export const prisma = new PrismaClient({
