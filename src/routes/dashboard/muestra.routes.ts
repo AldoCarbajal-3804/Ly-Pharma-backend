6cc695b56b19b7ab34120e6e29d69293c9f2d3f0
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-26 11:02:04 -0500

    Rutas de muestra

--- src/routes/dashboard/muestra.routes.ts
+++ src/routes/dashboard/muestra.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import { MuestraController } from "../../controller/dashboard/MuestraController"
 import { authenticate } from "../../middleware/authenticate"
+import { authorize } from "../../middleware/authorize"
 
 export const muestraRouter = Router()
 
-muestraRouter.get("/", authenticate, MuestraController.index)
+muestraRouter.get("/", authenticate, authorize("Admin", "Empleado"), MuestraController.index)
