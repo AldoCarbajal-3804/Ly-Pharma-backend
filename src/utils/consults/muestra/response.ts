d320fe9b33169327e6107e2c2f050cba9bd08bd4
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-26 10:56:38 -0500

    Response mejorada de muestre

--- src/utils/consults/muestra/response.ts
+++ src/utils/consults/muestra/response.ts
@@ -1,6 +1,13 @@
-export interface MuestraResponse {
+export interface EmployeeSummary {
     total_ventas_dia: number
+    ganancia_diaria: number
     stock_bajo: number
     productos_por_vencer: number
+}
+
+export interface AdminSummary {
+    total_productos_vendidos: number
     ganancia_diaria: number
+    stock_bajo: number
+    productos_por_vencer: number
 }
